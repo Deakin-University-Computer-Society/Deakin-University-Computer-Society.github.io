1839f73f91656b115ddd439395de4de8d37ca197
Rad_Quincy <hello@radquincy.com>
2024-11-29 14:37:22 +1100

    Updated to add partners

---
+++ src/app/TempHomeContent/Sponsors.tsx
@@ -0,0 +1,13 @@
+export function Sponsors() {
+	return (
+		<div className="flex flex-col gap-4">
+			<h2 className="text-2xl font-bold">Sponsors & Partners</h2>
+			<p>
+				DUCS is currently looking for sponsors to help cover the costs
+				running the society and/or work on projects together. If you are
+				interested in sponsoring, suporting or partnering with DUCS,
+				please contact our general email or presidents email.
+			</p>
+		</div>
+	);
+}
