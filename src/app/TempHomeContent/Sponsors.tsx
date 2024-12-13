8d1ded9490c74851bfd077cbd73bebcb1c2b3d2d
Rad_Quincy <hello@radquincy.com>
2024-12-13 14:12:14 +1100

    Spelling + Grammar and Where we run info

--- src/app/TempHomeContent/Sponsors.tsx
+++ src/app/TempHomeContent/Sponsors.tsx
@@ -4,9 +4,10 @@ export function Sponsors() {
 			<h2 className="text-2xl font-bold">Sponsors & Partners</h2>
 			<p>
 				DUCS is currently looking for sponsors to help cover the costs
-				running the society and/or work on projects together. If you are
-				interested in sponsoring, suporting or partnering with DUCS,
-				please contact our general email or presidents email.
+				of running the society and/or work on projects together. If you
+				are interested or know anyone who may be interseted in
+				sponsoring, suporting or partnering with DUCS, please contact us
+				at a relevant email below.
 			</p>
 		</div>
 	);
