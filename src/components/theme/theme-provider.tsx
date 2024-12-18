52b13a9f13e90e22d58025359d94b2f96fe0ac28
Rad_Quincy <hello@radquincy.com>
2024-12-18 13:39:02 +1100

    Fixed on Load flash of Red on Geko based browsers
    I don't know why it was occurring, but it was annoying me so I just removed the April fools feature I added.

--- src/components/theme/theme-provider.tsx
+++ src/components/theme/theme-provider.tsx
@@ -1,83 +1,66 @@
-import { createContext, useEffect, useState } from "react"
+import { createContext, useEffect, useState } from "react";
 
-type Theme = "dark" | "light" | "system" | "AprilFools"
+type Theme = "dark" | "light" | "system" | "AprilFools";
 
 type ThemeProviderProps = {
-    children: React.ReactNode
-    defaultTheme?: Theme
-    storageKey?: string
-}
+	children: React.ReactNode;
+	defaultTheme?: Theme;
+	storageKey?: string;
+};
 
 type ThemeProviderState = {
-    theme: Theme
-    setTheme: (theme: Theme) => void
-}
+	theme: Theme;
+	setTheme: (theme: Theme) => void;
+};
 
 const initialState: ThemeProviderState = {
-    theme: "system",
-    setTheme: () => null,
-}
+	theme: "system",
+	setTheme: () => null,
+};
 
-
-function isItAprilFoolDay() {
-    const aprilFools = {
-        month: 3,
-        date: 1
-    }
-
-    const now = new Date();
-    return (now.getMonth() == aprilFools.month && now.getDate() == aprilFools.date);
-}
-
-export const ThemeProviderContext = createContext<ThemeProviderState>(initialState)
+export const ThemeProviderContext =
+	createContext<ThemeProviderState>(initialState);
 
 export function ThemeProvider({
-    children,
-    defaultTheme = "system",
-    storageKey = "ui-theme",
-    ...props
+	children,
+	defaultTheme = "system",
+	storageKey = "ui-theme",
+	...props
 }: ThemeProviderProps) {
-    const [theme, setTheme] = useState<Theme>(
-        () => (localStorage.getItem(storageKey) as Theme) || defaultTheme
-    )
+	const [theme, setTheme] = useState<Theme>(
+		() => (localStorage.getItem(storageKey) as Theme) || defaultTheme
+	);
 
-    useEffect(() => {
-        const root = window.document.documentElement
+	useEffect(() => {
+		const root = window.document.documentElement;
 
-        root.classList.remove("light", "dark")
+		root.classList.remove("light", "dark");
 
+		if (theme === "system") {
+			const systemTheme = window.matchMedia(
+				"(prefers-color-scheme: dark)"
+			).matches
+				? "dark"
+				: "light";
 
-        if (isItAprilFoolDay()) {
-            root.classList.add("AprilFools")
-            console.log("Happy April Fools!!!")
-            return
-        }
+			root.classList.add(systemTheme);
+			return;
+		}
 
-        if (theme === "system") {
-            const systemTheme = window.matchMedia("(prefers-color-scheme: dark)")
-                .matches
-                ? "dark"
-                : "light"
+		root.classList.add(theme);
+	}, [theme]);
 
+	const value = {
+		theme,
+		setTheme: (theme: Theme) => {
+			localStorage.setItem(storageKey, theme);
+			setTheme(theme);
+		},
+	};
 
-            root.classList.add(systemTheme)
-            return
-        }
-
-        root.classList.add(theme)
-    }, [theme])
-
-    const value = {
-        theme,
-        setTheme: (theme: Theme) => {
-            localStorage.setItem(storageKey, theme)
-            setTheme(theme)
-        },
-    }
-
-    return (
-        <ThemeProviderContext.Provider {...props} value={value}>
-            {children}
-        </ThemeProviderContext.Provider>
-    )
+	return (
+		<ThemeProviderContext.Provider {...props} value={value}>
+			{children}
+		</ThemeProviderContext.Provider>
+	);
 }
